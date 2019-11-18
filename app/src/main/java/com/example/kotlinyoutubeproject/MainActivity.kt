8e9aa94cfe665f35acc2e8bcc2aab6b0c256b2b0
JosueDurand96 <josue.durand8@gmail.com>
2019-11-18 11:29:50 -0500

    a

--- app/src/main/java/com/example/kotlinyoutubeproject/MainActivity.kt
+++ app/src/main/java/com/example/kotlinyoutubeproject/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.kotlinyoutubeproject
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : YouTubeBaseActivity() {
 
+    companion object {
+        val VIDEO_ID: String = "YqggV_wGhM0";
+        val YOUTUBE_API_KEY: String = "AIzaSyAGYLzqvpEt3RECmwamycY217516f6frug"
+    }
+
+    lateinit var youtubePlayerInit: YouTubePlayer.OnInitializedListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initUI()
     }
+
+
+
 }
